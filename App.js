175fb4a1ff3c41482afcaaf73c6f1cf622660909
SanjaySharma <sharma.sanjay813@gmail.com>
2023-05-19 08:32:32 +0530

    JSX element, JSX functional coponent
    props
    
    project
    
    restraunt website

--- App.js
+++ App.js
@@ -1,29 +1,100 @@
-// const heading = React.createElement('h1', {id:'heading'}, 'I am H1'); // just creating a h1
+import React from "react";
+import ReactDOM  from "react-dom";
 
-//Now ahieve this
 /*
-    <div id="parent">
-        <div id="child">
-            <div id="sibling2">
-                <h1>Hello i am H1</h1>
-            </div>
-            <div id="sibling1">
-                <h2>Helllo i am H2</h2>
-            </div>
-        </div>
-    </div>
+Componenets required":
 
+1 Header
+    - Logo
+    - Nav braces
+2 Body
+    - Search
+    - Restraunt container 
+        - Restraunt cards // when there is cnace of code repating build separate component\
+            - image
+            - name of rest
+            - star rating
+            - cusinies
+            - estimate time 
+3 Footer
+    - Copyright
+    - Links
+    - Adress
+    - Contacts
+*/
+const Header = () => {
+    return (
+        <div className="header">
+        <div className="logo">
+            <img className="res-img" src="https://i.pinimg.com/736x/69/00/4b/69004bd4fa18768aa5611defc0f50dcf.jpg"/>
+        </div>
+        <div className="nav-items">
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li> Contact Us</li>
+                <li>Cart</li>
+            </ul>
+        </div>
+    </div>)
+}
+
+/*
+
+inline style in react 
+
+
+way 1: 
+const css = {
+    backgroundColor : '#f0f0f0',
+    width: '200px',
+    height:' 300px',
+    border: '1px solid black',
+}; && pass css to style={}
+
+way 2 : give js object to style={({css})}
 */
 
-const parent  = React.createElement('div', {id:'parent'},
-            React.createElement('div', {id:'child'},
-            [React.createElement('div', {id:'sibling1'},
-            React.createElement('h1', {}, 'I am H1' )),
-            React.createElement('div', {id:'sibling2'},
-            React.createElement('h2', {}, 'I am H2' ))] ));
+const RestrauntCard = (props) => {
+    // can also use destructuring 
+    const {resName, cuisines, stars, deliveryTime} = props; // what props here ctually is {resName:"Sanjay Foods", cuisines:"Dosa, South Indian, Asia", stars:"4.4 stars", deliveryTime:"38 minutes delivery time"} for res casrd 1.
+    return(
+        <div className="res-card">
+            <img
+            className="res-logo"
+            src="https://images.alphacoders.com/862/thumb-1920-862639.jpg"
+            />
+            <h3>{resName || props.resName}</h3>
+            <h4>{cuisines || props.cuisines}</h4>
+            <h4>{props.stars}</h4>
+            <h4>{props.deliveryTime}</h4>
+        </div>
+    )
+}
 
-// so root is hwere u want react to render item to!!!! it cna be whole doc or some picec of dom
-const root = ReactDOM.createRoot(document); 
-root.render(parent); // render will replae everything inside the root and put new stuff
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search
+                <input></input>
+            </div>
+            <div className="res-constainer">
+                <RestrauntCard resName="Sanjay Foods" cuisines="Dosa, South Indian, Asia" stars="4.4 stars" deliveryTime="38 minutes delivery time"/>
+                <RestrauntCard resName="KFC" cuisines="Burger, Fastfoods, Drinks" stars="4.4 stars" deliveryTime="38 minutes delivery time"/>
+            </div>
+        </div>
+    )
+}
 
+const AppLayout = () => {
+    return (
+    <div className="app">
+        <Header/>
+        <Body/>
+    </div>
+    )
+}
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<AppLayout/>); //we cant render directly
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # ReactJs
+
+# Parcel
+
+Dev Build
+Local Server
+HMR = Hot Module Replacement.
+File Watching Algorithm written in c++
+Caching - Faster Builds.
+Image Optimization
+Minification.
+Bundling.
+Compressing.
+Consistent Hashing --- read about it parcel.com 
+Code splitting
+Differential Bundling. - support older browser
+Diagnostics
+Error Handling
+HTTPs
+Tree Shaking- removes unused code
+Different buld for dev and prod build

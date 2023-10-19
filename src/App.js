e38cdcee3080770e008e1e03a7bdfc7ca5fc5c04
Sanjay Sharma <sharma.sanjay813@gmail.com>
2023-10-19 23:33:17 +0530

    createBrowserRouter, RouterProvider from react-router-dom has been introduced, the concept of routing begins fromt this Commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM  from "react-dom";
 import { Header } from "./components/Header";
 import { Body } from "./components/Body";
+import {About} from "./components/About";
+import {Contact} from "./components/Contact";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// RouterProvider is a context provider that provides routing context to all descendants.
 
 const AppLayout = () => {
     return (
@@ -12,6 +17,21 @@ const AppLayout = () => {
     )
 }
 
+const appRouter = createBrowserRouter(
+    [
+        { path: "/", element: <AppLayout/> },
+        { path: "/about", element: <About/> },
+        {path: "/contact", element: <Contact/>},
+    //     { path: "/contact", element: <Contact/> },
+    //     { path: "*", element: <NotFound/> },
+    ]
+    )
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+
+// Initally we were rendering the AppLayout directly
 root.render(<AppLayout/>); //we cant render directly
+
+// We will use router provider to provide routing context to all components.
+// so change root.render(<AppLayout/>)  --> root.render(<RouterProvider router={appRouter}><AppLayout/></RouterProvider>)

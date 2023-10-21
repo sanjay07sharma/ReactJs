b74486fa9059105c225db1e1add644a7474741cf
Sanjay Sharma <sharma.sanjay813@gmail.com>
2023-10-21 20:16:30 +0530

    added routes to site  	- / - /about - /contact

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+export const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <h2>This is the react about page.</h2>
+        </div>
+    );
+}
+
+// export default About;

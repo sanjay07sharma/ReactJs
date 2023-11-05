e16d363eb175229e58cafce5b5ad90ee8be3e441
Sanjay Sharma <sharma.sanjay813@gmail.com>
2023-11-05 11:00:40 +0530

    Let get Classy: 	- Difference between class component and functional component. 	- added tow components for understandings sake 	- count functionaity is added just to showcase the usage of state variable 	in class component.

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
+import User from './User';
+import { UserClass } from './UserClass';
+
 export const About = () => {
     return (
         <div>
             <h1>About</h1>
             <h2>This is the react about page.</h2>
+            {/* {this is an example of how to pass props in functional and class based Component in react} */}
+            <User name={"Sanjay Sharma (function)"}/>
+            {/* Loading a class based Component means i am creating an instance of that class*/}
+            <UserClass name={"Sanjay Sharma (class)"}/>
         </div>
     );
 }

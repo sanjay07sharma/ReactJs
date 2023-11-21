01835ded79bd182dc52065d2273c3b2557c1d5a9
Sanjay Sharma <sharma.sanjay813@gmail.com>
2023-11-21 09:00:10 +0530

    Configured tailwind css , replacing the traditional usage of css

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}",], // applied for these files extensions
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

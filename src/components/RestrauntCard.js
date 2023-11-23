de81dc588c929adfdef55fd7a56fac9758c94a2e
Sanjay Sharma <sharma.sanjay813@gmail.com>
2023-11-23 08:41:23 +0530

    Round 2 tailwind more to add

--- src/components/RestrauntCard.js
+++ src/components/RestrauntCard.js
@@ -17,9 +17,9 @@ export const RestrauntCard = (props) => {
       // Todo: add item to plate and update on card
 
   return (
-    <div className="res-card m-4 p-4 w-[250px] bg-gray-100">
-      <img className="res-logo" src={CDN_URL + cloudinaryImageId} />
-      <h3>{name}</h3>
+    <div className="res-card m-4 p-4 w-[250px] l-[450px] rounded-lg bg-gray-100 hover:w-[300px]">
+      <img className="res-logo rounded-lg" src={CDN_URL + cloudinaryImageId} />
+      <h3 className="font-bold py-4 text-lg">{name}</h3>
       <h4 class="cuisines">{cuisines.join(", ")}</h4>
       <h4>{avgRating}</h4>
       <h4>{costForTwo}</h4>

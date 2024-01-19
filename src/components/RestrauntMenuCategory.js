9ab99692bad3b4e1a775f65f1717785bbf6ab41d
Sanjay Sharma <sharma.sanjay813@gmail.com>
2024-01-19 22:55:08 +0530

    Added some UI improvement for RestrauntmenuCategory

--- src/components/RestrauntMenuCategory.js
+++ src/components/RestrauntMenuCategory.js
@@ -6,21 +6,22 @@ export const RestrauntMenuCategory = (menu) => {
       return (
         <div key={index} className="flex flex-wrap justify-center">
           {
-            item.card.card.itemCards.map((itemCard, index) => {
-              return (
-                <div key={index} className="m-4 bg-white rounded shadow-lg w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5">
-                  <div className="px-6 py-4">
-                    <div className="font-bold text-xl mb-2">{itemCard.card.info.category}</div>
-                    <p className="text-gray-700 text-base">{itemCard.card.info.description}</p>
-                  </div>
-                  <img className="w-full" src={CDN_URL + itemCard.card.info.imageId} alt={itemCard.card.info.name} />
-                  <div className="px-6 py-4">
-                    <p className="text-gray-700 text-base">{itemCard.card.info.name}</p>
-                    <p className="font-bold text-xl">Price: Rs. {itemCard.card.info.price / 100}</p>
-                  </div>
+            <div className="font-bold text-xl mb-2 item-category p-4">{item.card.card.title}
+                <div className="flex flex-wrap p-4">
+                    {
+                        item.card.card.itemCards.map((itemCard, index) => {
+                        return (
+                            <div key={index} className="m-4 p-4 bg-white rounded shadow-lg w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5">
+                                    <p className="text-gray-700 text-base">{itemCard.card.info.description}</p>
+                                    <img className="w-full" src={CDN_URL + itemCard.card.info.imageId} alt={itemCard.card.info.name} />
+                                    <p className="text-gray-700 text-base">{itemCard.card.info.name}</p>
+                                    <p className="font-bold text-xl">Price: Rs. {itemCard.card.info.price / 100}</p>
+                            </div>
+                        );
+                    })
+                    }
                 </div>
-              );
-            })
+            </div>
           }
         </div>
       );

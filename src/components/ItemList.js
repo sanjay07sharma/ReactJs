580d30155a42cdba1bff9419e3362ca90765d82a
Sanjay Sharma <sharma.sanjay813@gmail.com>
2024-01-26 23:03:30 +0530

    Modified accordian to look like swiggy. Added image and button to it. Added dropdown accordian functionality.

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,20 +1,29 @@
+import { CDN_URL } from "../utils/constants";
 
 const ItemList = ({...items}) => {
     return (
         <div>
             {items.item.map((item) => {
                 return (
-                    <div key={item.card.info.id}>
-                        <h3>{item.card.info.name}</h3>
-                        <p>{item.card.info.price}</p>
-                        <p>{item.card.info.description}</p>
-                        <img src={item.card.info.image} alt={item.card.info.title} />
+                    <div key={item.card.info.id} className="p-2 m-2  border-b-2 text-left flex justify-between">
+                        <div className="w-9/12">
+                            <div className="py-2">
+                                <span>{item.card.info.name}</span>
+                                <span> - ₹ {item.card.info.price ? (item.card.info.price)/100 : (item.card.info.defaultPrice)/100}</span>
+                            </div>
+                            <p className="text-xs overflow-wrap">{item.card.info.description}</p>
+                        </div>
+                        <div className="w-3/12 p-4">
+                            <div className="absolute">
+                                <button className="p-2 mx-16 bg-black text-white shadow-lg">Add +</button>
+                            </div>
+                            <img src={CDN_URL + item.card.info.imageId} alt={item.card.info.title} className="w-full"/>
+                        </div>
                     </div>
                 )
             })}  
         </div>
     )
-    
 }
 
 export default ItemList;

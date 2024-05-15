bd626e558178d0bc93844a1469e2cd77ecd991d3
Sanjay Sharma <sharma.sanjay813@gmail.com>
2024-05-15 22:33:59 +0530

    it/test, grouping test case

---
+++ src/components/__test__/contactUs.test.js
@@ -0,0 +1,31 @@
+import Contact from "../Contact";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+describe("Contact Component", () => {
+    test("should render the contactus component", () =>{
+        render(<Contact/>);
+        const button = screen.getByRole("button");
+        expect(button).toBeInTheDocument();
+    });
+    
+    // it and test are one and the samme thing.
+    it("should find submit button the contactus component", () =>{
+        render(<Contact/>);
+        const submitText = screen.getByText("Submit");
+        expect(submitText).toBeInTheDocument();
+    });
+    
+    it("should load the input name inside the contactus component", () =>{
+        render(<Contact/>);
+        const submitText = screen.getByPlaceholderText("Enter your name");
+        expect(submitText).toBeInTheDocument();
+    });
+    
+    it("should load the input name inside the contactus component", () =>{
+        render(<Contact/>);
+        const inputBoxed = screen.getAllByRole("textbox") // screen.getAllByRole('input') this does not exist, it textbox
+        // console.log(inputBoxed); // react object HTMLElement will be returned
+        expect(inputBoxed.length).toBe(3);
+    });
+});

8c3025bc4b9d6360e7a6012a44c3afd26150dea0
Sanjay Sharma <sharma.sanjay813@gmail.com>
2024-05-30 23:12:23 +0530

    Added functionality that fetches the data according to the image thats clicked needs more updations and different apporaches to be tried to display data to body component.

--- src/components/DisplayBoard.js
+++ src/components/DisplayBoard.js
@@ -1,8 +1,24 @@
 
-import { Link } from "react-router-dom";
-import { CDN_URL } from "../utils/constants";
-const displayAllRelated = (e, data) => {
-    // update <Body> component with all the data that is related to e.target data
+import { Link, json } from "react-router-dom";
+import { CDN_URL, FILTER_API } from "../utils/constants";
+import { useState, useEffect } from "react";
+
+const displayAllRelated = (e) => {
+    let url = e.target.src;
+    let parts = url.split('/');
+    let lastPart = parts.pop();
+    let fileNameParts = lastPart.split('.')[0].split('_');
+    let fileName = fileNameParts.join('');
+
+    const fetchFilteredData = async () => {
+        const data = await fetch(FILTER_API+fileName+`&sortBy=&filters=&type=rcv2&offset=0&page_type=null`);
+        const json = await data.json();
+        // Next Step is to try to find a way to display the data in REPLACING BODY component
+        
+        // Approach 1: Using Local Storage
+        // Approach 2: Replace everything in the body component with the new data.
+    }
+    fetchFilteredData();
 }
 const displayBoard = (props)  => {
     return props.data.map((element) => (
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,3 +5,6 @@ export const LOGO_URL =  "https://i.pinimg.com/736x/69/00/4b/69004bd4fa18768aa56
 export const RESTRAUNT_LIST_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.0224734&lng=72.5715931&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=";
+
+export const FILTER_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.02760&lng=72.58710&collection=83640&tags=layout_CCS_$"
+
